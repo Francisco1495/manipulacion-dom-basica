86e83ecac03fd247141639138aa1368dda51f978
Hector Valenzuela <francisco1495@gmail.com>
2023-02-27 07:14:35 -0700

    Agregar .gitignore y primeros archivos del proyecto

--- script.js
+++ script.js
@@ -6,10 +6,30 @@ var input1 = document.getElementById('calculo1');
 var input2 = document.getElementById('calculo2');
 var btn = document.getElementById('btnCalcular')
 var pResult = document.getElementById('result')
+var form = document.getElementById('form')
 
-//Como ves tenemos dos inputs y un boton que dice "calcular" abajo tgenemos creada una funcion que es donde vamos a sumar los dos valores puestos en los inputs y el boton hara que el resultado salga con un text que dice "El Resultado es: ", nota que creamos una etiqueta de parrafo en el HTML la cual esta vacia
+//Como ves tenemos dos inputs y un boton que dice "calcular" abajo tgenemos creada una funcion que es donde vamos a sumar los dos valores puestos en los inputs y el boton hara que el resultado salga con un text que dice "El Resultado es: ", nota que creamos una etiqueta de parrafo en el HTML la cual esta vacia.
 
-function btnOnclick(){
+form.addEventListener('click', sumarInputValues);
+
+//Fijate que hemos agregado los add event listener el cual nos dice cuando un suario h
+
+function sumarInputValues(event){
+    //console.log({event});
+    //event.preventDefault();
     var sumaInputs = (Number(input1.value) + Number(input2.value));
     pResult.innerHTML = "El Resultado es: " + sumaInputs;
 };
+
+h1.addEventListener("mouseover", pasarmouse);
+
+function pasarmouse(event){
+    console.log({event});
+    h1.onmouseover = h1.style.color = 'red'
+}
+
+h1.addEventListener("mouseout", quitarmouse);
+
+function quitarmouse(){
+    h1.onmouseout = h1.style.color = 'black'
+}
